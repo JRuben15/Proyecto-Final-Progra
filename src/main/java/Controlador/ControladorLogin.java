9d63f8af9f6969641d167eea3c9adb011b2686c4
Rubén Delgado C <Rubn Delgado C@LAPTOP-BBVIJ6IF>
2024-06-19 01:09:32 -0600

    Actualización Login (Incompleto) y Controlador

---
+++ src/main/java/Controlador/ControladorLogin.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Modelo.Usuarios;
+import Vista.FRM_Login;
+import Vista.FRM_Menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author
+ */
+public class ControladorLogin implements ActionListener {
+    private FRM_Login ventanaLogin;
+    private FRM_Menu ventanaMenu;
+    private Usuarios usuarios;
+    private UsuariosJpaController usuariosController;
+    
+    
+    public ControladorLogin()
+    {
+        this.ventanaLogin = new FRM_Login();
+        this.ventanaMenu = new FRM_Menu();
+        this.usuarios = new Usuarios();
+        this.usuariosController = new UsuariosJpaController();
+        
+        ventanaLogin.escuchador(this);
+        ventanaLogin.setVisible(true);
+    }
+    
+     public void cargarVentana(){
+        ventanaLogin.setVisible(true);
+    }
+     
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        switch(ae.getActionCommand().toString()) {
+            case "Ingresar":
+                ventanaLogin.configurarLogin(usuarios, usuarios.getUsuario(), usuarios.getContrasenna());
+                ventanaMenu.setVisible(true);
+            break;
+            
+            case "Salir":
+                ventanaLogin.mostrarMensaje("Saliendo...");
+                System.exit(0);
+            break;
+            
+            default:
+                throw new AssertionError();
+        }
+    }
+}//Fin CLASS
